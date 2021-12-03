d96dd29062020db72c2fe1e7b3a06156315a7c6d
Jessica Yang <jessicaawhy@gmail.com>
2021-12-02 22:58:01 -0800

    Init - Set up basic project configurations

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/dist'),
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
+};
